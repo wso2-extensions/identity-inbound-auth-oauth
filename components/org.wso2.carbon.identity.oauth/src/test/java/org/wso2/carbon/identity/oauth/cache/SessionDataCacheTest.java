599ae6811790c0051b9dd936b72ebcff3d198c6e
isuri97 <isurianuradha96@gmail.com>
2017-10-19 14:26:21 +0530

    Unit Testing for oauth_cache component

---
+++ components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth/cache/SessionDataCacheTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.oauth.cache;
+
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.testng.IObjectFactory;
+import org.testng.annotations.ObjectFactory;
+import org.testng.annotations.Test;
+import org.wso2.carbon.identity.application.common.cache.BaseCache;
+import org.wso2.carbon.utils.CarbonUtils;
+
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.testng.Assert.*;
+
+@PrepareForTest({CarbonUtils.class})
+public class SessionDataCacheTest {
+    @Mock
+    BaseCache baseCache;
+
+    @Mock
+    SessionDataCache sessionDataCache;
+
+    @ObjectFactory
+    public IObjectFactory getObjectFactory() {
+        return new org.powermock.modules.testng.PowerMockObjectFactory();
+    }
+
+    @Test
+    public void testGetInstance() throws Exception {
+        mockStatic(CarbonUtils.class);
+        CarbonUtils carbonUtils = new CarbonUtils();
+        assertNotEquals(SessionDataCache.getInstance(),carbonUtils,"Message not equal");
+    }
+
+    @Test
+    public void testAddToCache() throws Exception {
+        mockStatic(SessionDataCache.class);
+        when(SessionDataCache.getInstance()).thenReturn(sessionDataCache);
+        baseCache = mock(BaseCache.class);
+
+    }
+
+    @Test
+    public void testGetValueFromCache() throws Exception {
+    }
+
+    @Test
+    public void testClearCacheEntry() throws Exception {
+    }
+
+}
