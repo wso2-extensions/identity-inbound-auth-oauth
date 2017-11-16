6e9872c84df395d42155cc62faf85c59360313b1
Ruwan Abeykoon <ruwana@wso2.com>
2017-11-16 14:56:06 +0530

    Moved some tests to annotation based test framework.

--- components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/internal/OAuthTenantMgtListenerImplTest.java
+++ components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/internal/OAuthTenantMgtListenerImplTest.java
@@ -18,20 +18,16 @@
 
 package org.wso2.carbon.identity.oauth2.internal;
 
-import org.mockito.Mock;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
-import org.wso2.carbon.identity.oauth.OAuthUtil;
-import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
+import org.wso2.carbon.identity.common.testng.WithCarbonHome;
+import org.wso2.carbon.identity.common.testng.WithH2Database;
+import org.wso2.carbon.identity.common.testng.WithRealmService;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.oauth2.TestConstants;
-import org.wso2.carbon.identity.oauth2.dao.TokenMgtDAO;
 import org.wso2.carbon.identity.oauth2.model.AccessTokenDO;
 import org.wso2.carbon.identity.oauth2.model.AuthzCodeDO;
-import org.wso2.carbon.identity.testutil.powermock.PowerMockIdentityBaseTest;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -39,44 +35,21 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@PrepareForTest({OAuthTenantMgtListenerImpl.class, OAuthServerConfiguration.class, OAuthUtil.class})
-public class OAuthTenantMgtListenerImplTest extends PowerMockIdentityBaseTest {
-
-    @Mock
-    OAuthServerConfiguration oAuthServerConfiguration;
-
-    @BeforeMethod
-    public void setUp() throws Exception {
-        initMocks(this);
-        oAuthServerConfiguration = mock(OAuthServerConfiguration.class);
-        mockStatic(OAuthServerConfiguration.class);
-        when(OAuthServerConfiguration.getInstance()).thenReturn(oAuthServerConfiguration);
-    }
-
-    @AfterMethod
-    public void cleanUp() {
-        reset(oAuthServerConfiguration);
-    }
+@WithCarbonHome
+@WithH2Database(files = "dbScripts/identity.sql")
+@WithRealmService(injectToSingletons = IdentityTenantUtil.class)
+public class OAuthTenantMgtListenerImplTest {
 
     @DataProvider(name = "BuildAccessTokens")
-    public Object[][] buildAccessTokens() {
+    private Object[][] buildAccessTokens() {
         Set<AccessTokenDO> accessTokenDOSet = new HashSet<>();
         AccessTokenDO accessTokenDO = new AccessTokenDO();
         accessTokenDO.setAuthzUser(new AuthenticatedUser());
         accessTokenDOSet.add(accessTokenDO);
-        return new Object[][]{
-                {Collections.EMPTY_SET},
-                {accessTokenDOSet}
-        };
+        return new Object[][] { { Collections.EMPTY_SET }, { accessTokenDOSet } };
     }
 
     @Test(dataProvider = "BuildAccessTokens")
@@ -84,11 +57,6 @@ public class OAuthTenantMgtListenerImplTest extends PowerMockIdentityBaseTest {
         Set<AccessTokenDO> accessTokens = (Set<AccessTokenDO>) tokensSet;
         List<AuthzCodeDO> authzCodeDOList = new ArrayList<>();
         authzCodeDOList.add(new AuthzCodeDO());
-        TokenMgtDAO tokenMgtDAO = mock(TokenMgtDAO.class);
-        whenNew(TokenMgtDAO.class).withNoArguments().thenReturn(tokenMgtDAO);
-        mockStatic(OAuthUtil.class);
-        when(tokenMgtDAO.getAccessTokensOfTenant(TestConstants.TENANT_ID)).thenReturn(accessTokens);
-        when(tokenMgtDAO.getLatestAuthorizationCodesOfTenant(TestConstants.TENANT_ID)).thenReturn(authzCodeDOList);
         OAuthTenantMgtListenerImpl oAuthTenantMgtListener = spy(new OAuthTenantMgtListenerImpl());
         oAuthTenantMgtListener.onPreDelete(TestConstants.TENANT_ID);
         verify(oAuthTenantMgtListener).onPreDelete(TestConstants.TENANT_ID);
--- components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/internal/OAuthUserStoreConfigListenerImplTest.java
+++ components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/internal/OAuthUserStoreConfigListenerImplTest.java
@@ -18,20 +18,17 @@
 
 package org.wso2.carbon.identity.oauth2.internal;
 
-import org.mockito.Mock;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
-import org.wso2.carbon.identity.oauth.OAuthUtil;
-import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
+import org.wso2.carbon.identity.common.testng.WithCarbonHome;
+import org.wso2.carbon.identity.common.testng.WithH2Database;
+import org.wso2.carbon.identity.common.testng.WithRealmService;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.oauth2.TestConstants;
-import org.wso2.carbon.identity.oauth2.dao.TokenMgtDAO;
 import org.wso2.carbon.identity.oauth2.model.AccessTokenDO;
 import org.wso2.carbon.identity.oauth2.model.AuthzCodeDO;
-import org.wso2.carbon.identity.testutil.powermock.PowerMockIdentityBaseTest;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -39,38 +36,24 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@PrepareForTest({OAuthUserStoreConfigListenerImpl.class, OAuthServerConfiguration.class, OAuthUtil.class})
-public class OAuthUserStoreConfigListenerImplTest extends PowerMockIdentityBaseTest {
+@WithCarbonHome
+@WithH2Database(files = "dbScripts/identity.sql")
+@WithRealmService(injectToSingletons = IdentityTenantUtil.class)
+public class OAuthUserStoreConfigListenerImplTest {
 
     private OAuthUserStoreConfigListenerImpl oAuthUserStoreConfigListener;
     private static final String CURRENT_USER_STORE_NAME = "current";
     private static final String NEW_USER_STORE_NAME = "new";
 
-    @Mock
-    OAuthServerConfiguration oAuthServerConfiguration;
 
     @BeforeMethod
     public void setUp() throws Exception {
         oAuthUserStoreConfigListener = spy(new OAuthUserStoreConfigListenerImpl());
-        initMocks(this);
-        oAuthServerConfiguration = mock(OAuthServerConfiguration.class);
-        mockStatic(OAuthServerConfiguration.class);
-        when(OAuthServerConfiguration.getInstance()).thenReturn(oAuthServerConfiguration);
     }
 
-    @AfterMethod
-    public void tearDown() throws Exception {
-        reset(oAuthServerConfiguration);
-    }
 
     @DataProvider(name = "BuildAccessTokens")
     public Object[][] buildAccessTokens() {
@@ -87,11 +70,6 @@ public class OAuthUserStoreConfigListenerImplTest extends PowerMockIdentityBaseT
     @Test(dataProvider = "BuildAccessTokens")
     public void testOnUserStoreNamePreUpdate(Object tokensSet) throws Exception {
         Set<AccessTokenDO> accessTokens = (Set<AccessTokenDO>) tokensSet;
-        TokenMgtDAO tokenMgtDAO = mock(TokenMgtDAO.class);
-        whenNew(TokenMgtDAO.class).withNoArguments().thenReturn(tokenMgtDAO);
-        mockStatic(OAuthUtil.class);
-        when(tokenMgtDAO.getAccessTokensOfUserStore(TestConstants.TENANT_ID, CURRENT_USER_STORE_NAME)).thenReturn(
-                accessTokens);
         oAuthUserStoreConfigListener.onUserStoreNamePreUpdate(TestConstants.TENANT_ID, CURRENT_USER_STORE_NAME,
                 NEW_USER_STORE_NAME);
         verify(oAuthUserStoreConfigListener).onUserStoreNamePreUpdate(TestConstants.TENANT_ID, CURRENT_USER_STORE_NAME,
@@ -103,13 +81,6 @@ public class OAuthUserStoreConfigListenerImplTest extends PowerMockIdentityBaseT
         Set<AccessTokenDO> accessTokens = (Set<AccessTokenDO>) tokensSet;
         List<AuthzCodeDO> authzCodeDOList = new ArrayList<>();
         authzCodeDOList.add(new AuthzCodeDO());
-        TokenMgtDAO tokenMgtDAO = mock(TokenMgtDAO.class);
-        whenNew(TokenMgtDAO.class).withNoArguments().thenReturn(tokenMgtDAO);
-        mockStatic(OAuthUtil.class);
-        when(tokenMgtDAO.getAccessTokensOfUserStore(TestConstants.TENANT_ID, CURRENT_USER_STORE_NAME)).thenReturn(
-                accessTokens);
-        when(tokenMgtDAO.getLatestAuthorizationCodesOfUserStore(TestConstants.TENANT_ID, CURRENT_USER_STORE_NAME))
-                .thenReturn(authzCodeDOList);
         oAuthUserStoreConfigListener.onUserStorePreDelete(TestConstants.TENANT_ID, CURRENT_USER_STORE_NAME);
         verify(oAuthUserStoreConfigListener).onUserStorePreDelete(TestConstants.TENANT_ID, CURRENT_USER_STORE_NAME);
     }
