b0b74a9387b1f2bbbe02e6b1c48ac08d83309b70
senthalan <senthalank@gmail.com>
2018-02-15 17:22:49 +0530

    Change scope validator to run only the registered validator for a app

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/validators/DefaultOAuth2TokenValidator.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/validators/DefaultOAuth2TokenValidator.java
@@ -18,11 +18,20 @@
 
 package org.wso2.carbon.identity.oauth2.validators;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.oauth.common.exception.InvalidOAuthClientException;
 import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
+import org.wso2.carbon.identity.oauth.dao.OAuthAppDO;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.dto.OAuth2TokenValidationRequestDTO;
 import org.wso2.carbon.identity.oauth2.model.AccessTokenDO;
+import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -33,6 +42,7 @@ public class DefaultOAuth2TokenValidator implements OAuth2TokenValidator {
 
     public static final String TOKEN_TYPE = "bearer";
     private static final String ACCESS_TOKEN_DO = "AccessTokenDO";
+    private Log log = LogFactory.getLog(DefaultOAuth2TokenValidator.class);
 
     @Override
     public boolean validateAccessDelegation(OAuth2TokenValidationMessageContext messageContext)
@@ -43,32 +53,52 @@ public class DefaultOAuth2TokenValidator implements OAuth2TokenValidator {
     }
 
     @Override
-    public boolean validateScope(OAuth2TokenValidationMessageContext messageContext)
-            throws IdentityOAuth2Exception {
+    public boolean validateScope(OAuth2TokenValidationMessageContext messageContext) throws IdentityOAuth2Exception {
 
-        Set<OAuth2ScopeValidator> oAuth2ScopeValidators = OAuthServerConfiguration.getInstance()
-                .getOAuth2ScopeValidators();
-        for (OAuth2ScopeValidator validator: oAuth2ScopeValidators) {
+        ArrayList<String> appScopeValidators;
+        AccessTokenDO accessTokenDO = (AccessTokenDO) messageContext.getProperty(ACCESS_TOKEN_DO);
+        OAuthAppDO app;
+        try {
+            app = OAuth2Util.getAppInformationByClientId(accessTokenDO.getConsumerKey());
+            appScopeValidators = new ArrayList<>(Arrays.asList(app.getScopeValidators()));
+        } catch (InvalidOAuthClientException e) {
+            throw new IdentityOAuth2Exception(String.format("Identity Application Management Exception occurred when " +
+                    "getting app information for client id %s ", accessTokenDO.getConsumerKey()), e);
+        }
 
-            if (validator != null && validator.canHandle(messageContext)) {
-                String resource = null;
-                if (messageContext.getRequestDTO().getContext() != null) {
-                    //Iterate the array of context params to find the 'resource' context param.
-                    for (OAuth2TokenValidationRequestDTO.TokenValidationContextParam resourceParam :
-                            messageContext.getRequestDTO().getContext()) {
-                        //If the context param is the resource that is being accessed
-                        if (resourceParam != null && "resource".equals(resourceParam.getKey())) {
-                            resource = resourceParam.getValue();
-                            break;
+        if (!appScopeValidators.isEmpty()) {
+            Set<OAuth2ScopeValidator> oAuth2ScopeValidators = OAuthServerConfiguration.getInstance()
+                    .getOAuth2ScopeValidators();
+            for (OAuth2ScopeValidator validator : oAuth2ScopeValidators) {
+                if (validator != null && appScopeValidators.contains(validator.getClass().getSimpleName())
+                        && validator.canHandle(messageContext)) {
+                    String resource = null;
+                    if (messageContext.getRequestDTO().getContext() != null) {
+                        //Iterate the array of context params to find the 'resource' context param.
+                        for (OAuth2TokenValidationRequestDTO.TokenValidationContextParam resourceParam :
+                                messageContext.getRequestDTO().getContext()) {
+                            //If the context param is the resource that is being accessed
+                            if (resourceParam != null && "resource".equals(resourceParam.getKey())) {
+                                resource = resourceParam.getValue();
+                                break;
+                            }
                         }
                     }
+                    if (log.isDebugEnabled()) {
+                        log.debug(String.format("Validating scope of token %s using %s", accessTokenDO.getTokenId(),
+                                validator.getClass().getName()));
+                    }
+                    boolean isValid = validator.validateScope(accessTokenDO, resource);
+                    appScopeValidators.remove(validator.getClass().getSimpleName());
+                    if (!isValid) {
+                        return false;
+                    }
                 }
-                boolean isValid = validator.validateScope((AccessTokenDO) messageContext.getProperty
-                        (ACCESS_TOKEN_DO), resource);
-
-                if (!isValid) {
-                    return false;
-                }
+            }
+            if (!appScopeValidators.isEmpty()) {
+                throw new IdentityOAuth2Exception(String.format("The scope validators %s registered for application " +
+                                "%s@%s are not found in the server configuration ", StringUtils.join(appScopeValidators,
+                        ","), app.getApplicationName(), OAuth2Util.getTenantDomainOfOauthApp(app)));
             }
         }
         return true;
