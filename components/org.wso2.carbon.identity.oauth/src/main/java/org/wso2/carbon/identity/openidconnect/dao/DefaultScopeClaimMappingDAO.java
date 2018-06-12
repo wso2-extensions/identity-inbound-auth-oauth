271b44b83594fc45d8cbce91fa70572ace756a73
DMHP <hasanthidisanayakka@yahoo.com>
2018-06-12 09:41:59 +0530

    Implement DAO layer to store scopes and claims

---
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/openidconnect/dao/DefaultScopeClaimMappingDAO.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *   Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ * /
+ */
+
+package org.wso2.carbon.identity.openidconnect.dao;
+
+import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
+
+import java.util.Map;
+
+/**
+ * This interface handles all the DAO layer activities which are related to OIDC scope claim persistence.
+ */
+public interface DefaultScopeClaimMappingDAO {
+
+    /**
+     * To store oidc scopes.
+     *
+     * @param tenantId      tenant Id
+     * @param ScopeClaimMap oidc scopes claims mapping
+     * @throws IdentityOAuth2Exception
+     */
+    void insertAllScopes(int tenantId, Map<String, String> ScopeClaimMap, boolean isOnActivate) throws IdentityOAuth2Exception;
+
+    Map<String, String> loadScopesClaimsMapping(int tenantId) throws IdentityOAuth2Exception;
+
+}
