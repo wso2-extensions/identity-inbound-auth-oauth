57098acdccf5cfe894b0139be9186659b6519cb6
DMHP <hasanthidisanayakka@yahoo.com>
2018-07-28 16:17:44 +0530

    Move the OIDC discovery endpoint under the root <issuer>/.well-known/

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/oidcdiscovery/OIDCDiscoveryEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/oidcdiscovery/OIDCDiscoveryEndpoint.java
@@ -30,7 +30,7 @@ import org.wso2.carbon.identity.discovery.builders.OIDProviderResponseBuilder;
 import org.wso2.carbon.identity.oauth.common.OAuthConstants;
 import org.wso2.carbon.identity.oauth.endpoint.oidcdiscovery.impl.OIDProviderJSONResponseBuilder;
 import org.wso2.carbon.identity.oauth.endpoint.util.EndpointUtil;
-
+import java.net.URI;
 import java.net.URISyntaxException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -50,7 +50,6 @@ public class OIDCDiscoveryEndpoint {
     private static final Log log = LogFactory.getLog(OIDCDiscoveryEndpoint.class);
 
     @GET
-    @Path("/.well-known/openid-configuration")
     @Produces("application/json")
     public Response getOIDProviderConfiguration(@PathParam("issuer") String tokenEp, @Context HttpServletRequest request
             , @Context UriInfo uriInfo) {
@@ -68,16 +67,21 @@ public class OIDCDiscoveryEndpoint {
             tenantDomain = MultitenantConstants.SUPER_TENANT_DOMAIN_NAME;
         }
          /*  [https://openid.net/specs/openid-connect-discovery-1_0.html#IssuerDiscovery]
-             "OpenID Providers supporting Discovery MUST make a JSON document available at the path formed by
-             concatenating the string /.well-known/openid-configuration to the Issuer."*/
+             "OpenID Providers supporting Discovery MUST make a JSON document available at the path formed by concatenating
+             the string /.well-known/openid-configuration to the Issuer."*/
         try {
+
+            if (!MultitenantConstants.SUPER_TENANT_DOMAIN_NAME.equals(tenantDomain)) {
+                issuer = getTenantUrl(issuer, tenantDomain);
+            }
+
             if (getOidcDiscoveryEPUrl(tenantDomain).equals(issuer)) {
 
                 return this.getResponse(request, tenantDomain);
             } else {
                 Response.ResponseBuilder errorResponse = Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                return errorResponse.entity("Error while loading the endpoint. The path to the discovery document " +
-                        "should be in the format of {issuer}/.well-known/openid-configuration").build();
+                return errorResponse.entity("Error while loading the endpoint. The path to the discovery document should " +
+                        "be in the format of {issuer}/.well-known/openid-configuration").build();
             }
         } catch (URISyntaxException e) {
             Response.ResponseBuilder errorResponse = Response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -102,5 +106,12 @@ public class OIDCDiscoveryEndpoint {
         }
         Response.ResponseBuilder responseBuilder = Response.status(HttpServletResponse.SC_OK);
         return responseBuilder.entity(response).build();
+    }
+
+    private static String getTenantUrl(String url, String tenantDomain) throws URISyntaxException {
+        URI uri = new URI(url);
+        URI uriModified = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), ("/t/" +
+                tenantDomain + uri.getPath()), uri.getQuery(), uri.getFragment());
+        return uriModified.toString();
     }
 }
