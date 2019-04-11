66affbc0a21956fabec23615a934660816d58c7e
ayshsandu <ayeshad.09@cse.mrt.ac.lk>
2019-04-11 21:26:27 +0530

    Throw exception when request object is expired

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/openidconnect/RequestObjectValidatorImpl.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/openidconnect/RequestObjectValidatorImpl.java
@@ -115,7 +115,7 @@ public class RequestObjectValidatorImpl implements RequestObjectValidator {
         return validateAudience(tokenEPUrl, audience);
     }
 
-    private boolean checkExpirationTime(RequestObject requestObject) {
+    private boolean checkExpirationTime(RequestObject requestObject) throws RequestObjectException {
 
         Date expirationTime = requestObject.getClaimsSet().getExpirationTime();
         if (expirationTime != null) {
@@ -127,7 +127,9 @@ public class RequestObjectValidatorImpl implements RequestObjectValidator {
                         ", Expiration Time(ms) : " + expirationTimeInMillis +
                         ", TimeStamp Skew : " + timeStampSkewMillis +
                         ", Current Time : " + currentTimeInMillis + ". Token Rejected.";
-                return logAndReturnFalse(msg);
+                logAndReturnFalse(msg);
+                throw new RequestObjectException(RequestObjectException.ERROR_CODE_INVALID_REQUEST, "Request Object " +
+                        "is Expired.");
             }
         }
         return true;
