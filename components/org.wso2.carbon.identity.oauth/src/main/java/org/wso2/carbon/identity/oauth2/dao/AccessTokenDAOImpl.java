bf542e9beb78bca46279a28f2f43ae1ad59eb1eb
Dinushi <dinushiranagalage.15@cse.mrt.ac.lk>
2020-10-29 09:23:25 +0530

    Add preprocessing on the refresh token

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/dao/AccessTokenDAOImpl.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/dao/AccessTokenDAOImpl.java
@@ -853,7 +853,7 @@ public class AccessTokenDAOImpl extends AbstractOAuthDAO implements AccessTokenD
                     long validityPeriodInMillis = resultSet.getLong(8);
                     long refreshTokenValidityPeriodMillis = resultSet.getLong(9);
                     String tokenType = resultSet.getString(10);
-                    String refreshToken = resultSet.getString(11);
+                    String refreshToken = getPersistenceProcessor().getPreprocessedRefreshToken(resultSet.getString(11));
                     String tokenId = resultSet.getString(12);
                     String grantType = resultSet.getString(13);
                     String subjectIdentifier = resultSet.getString(14);
