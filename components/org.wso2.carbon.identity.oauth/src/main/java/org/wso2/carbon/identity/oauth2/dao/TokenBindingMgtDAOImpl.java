136a64e58914aa91023d3ad462beca41689eb24a
Janak Amarasena <janakamarasena@gmail.com>
2021-02-08 16:55:28 +0530

    Improve debug logs

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/dao/TokenBindingMgtDAOImpl.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/dao/TokenBindingMgtDAOImpl.java
@@ -87,6 +87,13 @@ public class TokenBindingMgtDAOImpl implements TokenBindingMgtDAO {
     @Override
     public void storeTokenBinding(TokenBinding tokenBinding, int tenantId) throws IdentityOAuth2Exception {
 
+        if (tokenBinding == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Token binding information is not available. " +
+                        "Returning without proceeding to store token binding information.");
+            }
+            return;
+        }
         if (log.isDebugEnabled()) {
             log.debug("Storing token binding information" +
                     " accessTokenId: " + tokenBinding.getTokenId() +
