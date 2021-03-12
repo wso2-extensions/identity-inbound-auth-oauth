4871d8ad59bb93a8ad1c93a635937627906dd12a
piraveena <piraveena18@gmail.com>
2021-03-12 17:17:44 +0530

    Remove tenantdomain from password grant error response

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/handlers/grant/PasswordGrantHandler.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/handlers/grant/PasswordGrantHandler.java
@@ -222,7 +222,12 @@ public class PasswordGrantHandler extends AbstractAuthorizationGrantHandler {
                         (tokenReq.getResourceOwnerUsername()))) {
                     throw new IdentityOAuth2Exception("Authentication failed for " + tenantAwareUserName);
                 }
-                throw new IdentityOAuth2Exception("Authentication failed for " + tokenReq.getResourceOwnerUsername());
+                String username = tokenReq.getResourceOwnerUsername();
+                if (IdentityTenantUtil.isTenantQualifiedUrlsEnabled()) {
+                    // For tenant qualified urls, no need to send fully qualified username in response.
+                    username = tenantAwareUserName;
+                }
+                throw new IdentityOAuth2Exception("Authentication failed for " + username);
             } else if (isPublishPasswordGrantLoginEnabled) {
                 publishAuthenticationData(tokenReq, true, serviceProvider);
             }
