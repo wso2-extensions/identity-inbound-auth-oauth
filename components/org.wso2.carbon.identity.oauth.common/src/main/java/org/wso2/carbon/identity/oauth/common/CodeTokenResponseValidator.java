5999b64b4cc672d3e433d3ec755f50e3f7f952bf
Thiloshon Nagarajah <thiloshon@gmail.com>
2021-05-12 12:42:11 +0530

    Update components/org.wso2.carbon.identity.oauth.common/src/main/java/org/wso2/carbon/identity/oauth/common/CodeTokenResponseValidator.java
    Co-authored-by: Farasath Ahamed <mefarazath@gmail.com>

--- components/org.wso2.carbon.identity.oauth.common/src/main/java/org/wso2/carbon/identity/oauth/common/CodeTokenResponseValidator.java
+++ components/org.wso2.carbon.identity.oauth.common/src/main/java/org/wso2/carbon/identity/oauth/common/CodeTokenResponseValidator.java
@@ -66,7 +66,7 @@ public class CodeTokenResponseValidator extends TokenValidator {
 
         if (StringUtils.isNotBlank(request.getParameter(REQUEST_URI))) {
             // PAR spec mandates request_uri to have client_id, response_type in accordance to OAuth 2.0
-            // Also request is disallowed if request_uri is used.
+            // Also 'request' parameter is disallowed if 'request_uri' parameter is present in the authorization request.
             requiredParams = new ArrayList<>(Arrays.asList(CLIENT_ID, RESPONSE_TYPE, REQUEST_URI));
             notAllowedParams.add(REQUEST);
         }
