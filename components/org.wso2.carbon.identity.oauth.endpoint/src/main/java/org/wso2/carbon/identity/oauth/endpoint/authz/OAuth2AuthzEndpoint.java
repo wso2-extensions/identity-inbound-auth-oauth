3bc4b0fcf06d7a2b737dda31825382bbb61e3353
Thiloshon Nagarajah <thiloshon@gmail.com>
2021-05-12 11:22:11 +0530

    pr changes

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -1691,9 +1691,10 @@ public class OAuth2AuthzEndpoint {
         overrideAuthzParameters(oAuthMessage, parameters, oauthRequest.getParam(REQUEST),
                 oauthRequest.getParam(REQUEST_URI), requestObject);
 
-        // if the redirect uri was not given in auth request, validating if the registration redirect uri was added
+        // If the redirect uri was not given in auth request the registered redirect uri will be available here,
+        // so validating if the registered redirect uri is a single uri that can be properly redirected.
         if (parameters.getRedirectURI().startsWith(REGEX_PATTERN)) {
-            throw new InvalidRequestException("Redirect URI is not present in the authorization request",
+            throw new InvalidRequestException("Redirect URI is not present in the authorization request.",
                     OAuth2ErrorCodes.INVALID_REQUEST, OAuth2ErrorCodes.OAuth2SubErrorCodes.INVALID_REDIRECT_URI);
         }
         persistRequestObject(parameters, requestObject);
