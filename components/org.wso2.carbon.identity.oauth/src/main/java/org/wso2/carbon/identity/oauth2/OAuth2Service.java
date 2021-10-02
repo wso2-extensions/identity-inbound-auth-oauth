33b3891a539544405d62be366f3f15d8fab83f57
piraveena <piraveena18@gmail.com>
2021-10-02 17:01:34 +0530

    Fix revocation flow

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2Service.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2Service.java
@@ -405,8 +405,13 @@ public class OAuth2Service extends AbstractAdmin {
                     if (StringUtils.isNotBlank(refreshTokenDO.getTokenBindingReference())) {
                         tokenBindingReference = refreshTokenDO.getTokenBindingReference();
                     }
+                    String tenantDomain = null;
+                    if (refreshTokenDO.getAuthorizedUser() != null) {
+                        tenantDomain = refreshTokenDO.getAuthorizedUser().getTenantDomain();
+                    }
                     OAuthUtil.clearOAuthCache(revokeRequestDTO.getConsumerKey(), refreshTokenDO.getAuthorizedUser(),
-                            OAuth2Util.buildScopeString(refreshTokenDO.getScope()), tokenBindingReference);
+                            OAuth2Util.buildScopeString(refreshTokenDO.getScope()), tokenBindingReference,
+                            tenantDomain);
                     OAuthUtil.clearOAuthCache(revokeRequestDTO.getConsumerKey(), refreshTokenDO.getAuthorizedUser(),
                             OAuth2Util.buildScopeString(refreshTokenDO.getScope()));
                     OAuthUtil.clearOAuthCache(revokeRequestDTO.getConsumerKey(), refreshTokenDO.getAuthorizedUser());
@@ -433,8 +438,13 @@ public class OAuth2Service extends AbstractAdmin {
                                 .isNotBlank(accessTokenDO.getTokenBinding().getBindingReference())) {
                             tokenBindingReference = accessTokenDO.getTokenBinding().getBindingReference();
                         }
+                        String tenantDomain = null;
+                        if (accessTokenDO.getAuthzUser() != null) {
+                            tenantDomain = accessTokenDO.getAuthzUser().getTenantDomain();
+                        }
                         OAuthUtil.clearOAuthCache(revokeRequestDTO.getConsumerKey(), accessTokenDO.getAuthzUser(),
-                                OAuth2Util.buildScopeString(accessTokenDO.getScope()), tokenBindingReference);
+                                OAuth2Util.buildScopeString(accessTokenDO.getScope()), tokenBindingReference,
+                                tenantDomain);
                         OAuthUtil.clearOAuthCache(revokeRequestDTO.getConsumerKey(), accessTokenDO.getAuthzUser(),
                                 OAuth2Util.buildScopeString(accessTokenDO.getScope()));
                         OAuthUtil.clearOAuthCache(revokeRequestDTO.getConsumerKey(), accessTokenDO.getAuthzUser());
