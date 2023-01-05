f182781c9ecaa02f1704e4557c51dd50a3c2103f
Farasath Ahamed <mefarazath@gmail.com>
2023-01-05 19:21:45 +0530

    Update components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/AccessTokenIssuer.java

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/AccessTokenIssuer.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/AccessTokenIssuer.java
@@ -490,7 +490,7 @@ public class AccessTokenIssuer {
         String grantType = tokenReqDTO.getGrantType();
         if (GrantType.AUTHORIZATION_CODE.toString().equals(grantType)) {
             /*
-             In authorization code flow, we have already done scope validation during the /authorize call an issued an
+             In authorization code flow, we have already completed scope validation during the /authorize call and issued an
              authorization code with the authorized scopes. During the token flow we only consider the scopes bound to the
              issued authorization code and simply ignore any 'scope' parameter sent in the subsequent token request.
              Therefore, it does not make sense to go through scope validation again as there will be no new scopes to
