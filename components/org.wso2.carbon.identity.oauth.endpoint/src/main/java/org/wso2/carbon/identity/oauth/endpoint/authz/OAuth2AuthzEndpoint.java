48200bb9a59290b4d4bbba6e39416436767973d6
ChitharaKarunasekera <chithara.2019160@iit.ac.lk>
2023-03-15 11:34:34 +0530

    check req_uri

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -257,7 +257,7 @@ public class OAuth2AuthzEndpoint {
                 FrameworkUtils.startTenantFlow(tenantDomain);
             }
 
-            if (isPassthroughToFramework(oAuthMessage)) {
+            if (request.getRequestURI() != null || isPassthroughToFramework(oAuthMessage)) {
                 return handleAuthFlowThroughFramework(oAuthMessage);
             } else if (isInitialRequestFromClient(oAuthMessage)) {
                 return handleInitialAuthorizationRequest(oAuthMessage);
