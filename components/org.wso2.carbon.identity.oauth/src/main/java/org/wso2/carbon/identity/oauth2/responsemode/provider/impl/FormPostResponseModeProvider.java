5bea11c72705005cca62c362aa4d8f90796f7c62
Anju Chamantha <chamantha97anju@gmail.com>
2023-06-02 09:30:41 +0530

    Remove possible duplications of params in form post response mode

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/responsemode/provider/impl/FormPostResponseModeProvider.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/responsemode/provider/impl/FormPostResponseModeProvider.java
@@ -80,19 +80,19 @@ public class FormPostResponseModeProvider extends AbstractResponseModeProvider {
                     .append("\"/>\n");
         }
 
-        if (StringUtils.isNotEmpty(authenticatedIdPs)) {
+        if (StringUtils.isNotEmpty(authenticatedIdPs) && !jsonObject.has("AuthenticatedIdPs")) {
             paramStringBuilder.append("<input type=\"hidden\" name=\"AuthenticatedIdPs\" value=\"")
                     .append(authenticatedIdPs)
                     .append("\"/>\n");
         }
 
-        if (StringUtils.isNotEmpty(sessionStateValue)) {
+        if (StringUtils.isNotEmpty(sessionStateValue) && !jsonObject.has("session_state")) {
             paramStringBuilder.append("<input type=\"hidden\" name=\"session_state\" value=\"")
                     .append(sessionStateValue)
                     .append("\"/>\n");
         }
 
-        if (StringUtils.isNotEmpty(state)) {
+        if (StringUtils.isNotEmpty(state) && !jsonObject.has("state")) {
             paramStringBuilder.append("<input type=\"hidden\" name=\"state\" value=\"")
                     .append(state)
                     .append("\"/>\n");
