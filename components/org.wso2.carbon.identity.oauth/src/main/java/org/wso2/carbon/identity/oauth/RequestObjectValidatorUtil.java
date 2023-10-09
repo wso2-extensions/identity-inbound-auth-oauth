d2aec4f6c1b1813b6bc7e3bd6012918bb67bcfb3
sachinisiriwardene <sachinisiriwardene@gmail.com>
2023-10-09 12:34:27 +0530

    Add support for request_object_signing_alg value sent in DCR

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/RequestObjectValidatorUtil.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/RequestObjectValidatorUtil.java
@@ -29,6 +29,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.common.model.ServiceProviderProperty;
 import org.wso2.carbon.identity.oauth.common.OAuth2ErrorCodes;
+import org.wso2.carbon.identity.oauth.common.exception.InvalidOAuthClientException;
+import org.wso2.carbon.identity.oauth.dao.OAuthAppDO;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.RequestObjectException;
 import org.wso2.carbon.identity.oauth2.model.OAuth2Parameters;
@@ -64,6 +66,19 @@ public class RequestObjectValidatorUtil {
         boolean isVerified;
         Certificate certificate = null;
         SignedJWT jwt = requestObject.getSignedJWT();
+        OAuthAppDO oAuthAppDO = null;
+        try {
+            oAuthAppDO = OAuth2Util.getAppInformationByClientId(oAuth2Parameters.getClientId(),
+                    oAuth2Parameters.getTenantDomain());
+            String algorithm = oAuthAppDO.getRequestObjectSignatureAlgorithm();
+            if (StringUtils.isNotEmpty(algorithm) && !algorithm.equals(jwt.getHeader().getAlgorithm().getName())) {
+                throw new RequestObjectException("Request Object signature verification failed. Invalid signature " +
+                        "algorithm.", OAuth2ErrorCodes.INVALID_REQUEST);
+            }
+        } catch (IdentityOAuth2Exception | InvalidOAuthClientException e) {
+            throw new RequestObjectException(OAuth2ErrorCodes.SERVER_ERROR, "Error while retrieving Oauth application "
+                    + "to check signature algorithm.", e);
+        }
         try {
             certificate =
                     getCertificateForAlias(oAuth2Parameters.getTenantDomain(), oAuth2Parameters.getClientId());
