465e8a259d7c8536043cea8521a2937eca992a13
Chinthaka Jayatilake <37581983+ChinthakaJ98@users.noreply.github.com>
2023-10-17 20:43:44 +0530

    Removing unused constant

--- components/org.wso2.carbon.identity.oauth.common/src/main/java/org/wso2/carbon/identity/oauth/common/OAuthConstants.java
+++ components/org.wso2.carbon.identity.oauth.common/src/main/java/org/wso2/carbon/identity/oauth/common/OAuthConstants.java
@@ -189,7 +189,6 @@ public final class OAuthConstants {
     public static final String IS_FAPI_CONFORMANT_APP = "isFAPIConformant";
     public static final String ENABLE_FAPI = "OAuth.OpenIDConnect.FAPI.EnableFAPIValidation";
     public static final String IS_THIRD_PARTY_APP = "isThirdPartyApp";
-    public static final String TOKEN_ENDPOINT_AUTH_METHOD = "token_endpoint_auth_method";
 
     private OAuthConstants() {
 
