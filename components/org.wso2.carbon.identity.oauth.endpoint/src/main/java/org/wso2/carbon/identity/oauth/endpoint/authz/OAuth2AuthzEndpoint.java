ad02aafee3b7aeefeaf830803c8aee78bba5abdc
sachinisiriwardene <sachinisiriwardene@gmail.com>
2023-10-26 20:59:24 +0530

    remove unnecessary code

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -2468,9 +2468,6 @@ public class OAuth2AuthzEndpoint {
                                     .OAuth2SubErrorCodes.INVALID_REQUEST_OBJECT, e.getErrorCode(), e.getErrorMessage(),
                             null, params);
                 }
-            } catch (InvalidRequestException e) {
-                return EndpointUtil.getErrorPageURL(oAuthMessage.getRequest(), e.getErrorCode(),
-                        e.getErrorCode(), e.getErrorMessage(), null, params);
             }
         }
 
