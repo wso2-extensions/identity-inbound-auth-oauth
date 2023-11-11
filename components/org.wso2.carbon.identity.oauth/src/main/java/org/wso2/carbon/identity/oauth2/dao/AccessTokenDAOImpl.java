df5fbcbd0781d645c7970655c2a576661e1179ba
sadilchamishka <sandilchamishka@gmail.com>
2023-11-11 07:12:58 +0530

    Fix Token binding expiry issue for the b2b users

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/dao/AccessTokenDAOImpl.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/dao/AccessTokenDAOImpl.java
@@ -2976,9 +2976,17 @@ public class AccessTokenDAOImpl extends AbstractOAuthDAO implements AccessTokenD
                             String subjectIdentifier = resultSet.getString("SUBJECT_IDENTIFIER");
                             String userDomain = resultSet.getString("USER_DOMAIN");
                             String authenticatedIDPName = resultSet.getString("NAME");
+                            String authorizedOrganization = resultSet.getString("AUTHORIZED_ORGANIZATION");
                             AuthenticatedUser user = OAuth2Util.createAuthenticatedUser(authzUser,
                                     userDomain, OAuth2Util.getTenantDomain(tenantId), authenticatedIDPName);
                             user.setAuthenticatedSubjectIdentifier(subjectIdentifier);
+                            if (user.isFederatedUser() &&
+                                    !OAuthConstants.AuthorizedOrganization.NONE.equals(authorizedOrganization)) {
+                                user.setAccessingOrganization(authorizedOrganization);
+                                user.setUserResidentOrganization(resolveOrganizationId(user.getTenantDomain()));
+                                // Tenant domain should be properly set for the organization SSO login users.
+                                user.setTenantDomain(OAuth2Util.getTenantDomain(IdentityTenantUtil.getLoginTenantId()));
+                            }
                             Timestamp issuedTime = resultSet
                                     .getTimestamp("TIME_CREATED", Calendar.getInstance(TimeZone.getTimeZone(UTC)));
                             Timestamp refreshTokenIssuedTime =
