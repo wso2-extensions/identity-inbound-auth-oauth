be13ff28578b4a64cbdf7ba485e3b64673172e90
Anju Chamantha <chamantha97anju@gmail.com>
2024-05-02 15:52:50 +0530

    Refactoring methods in DCRConfigurationMgtServiceImplTest.java

--- components/org.wso2.carbon.identity.oauth.dcr/src/test/java/org/wso2/carbon/identity/oauth/dcr/DCRConfigurationMgtServiceImplTest.java
+++ components/org.wso2.carbon.identity.oauth.dcr/src/test/java/org/wso2/carbon/identity/oauth/dcr/DCRConfigurationMgtServiceImplTest.java
@@ -18,39 +18,31 @@
 
 package org.wso2.carbon.identity.oauth.dcr;
 
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.wso2.carbon.caching.impl.DataHolder;
 import org.wso2.carbon.identity.configuration.mgt.core.ConfigurationManager;
-import org.wso2.carbon.identity.configuration.mgt.core.ConfigurationManagerImpl;
 import org.wso2.carbon.identity.configuration.mgt.core.model.Attribute;
 import org.wso2.carbon.identity.configuration.mgt.core.model.Resource;
-import org.wso2.carbon.identity.configuration.mgt.core.model.ResourceAdd;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth.common.OAuthConstants;
 import org.wso2.carbon.identity.oauth.dcr.exception.DCRMException;
-import org.wso2.carbon.identity.oauth.dcr.exception.DCRMServerException;
 import org.wso2.carbon.identity.oauth.dcr.internal.DCRDataHolder;
 import org.wso2.carbon.identity.oauth.dcr.model.DCRConfiguration;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.reflect.Whitebox.invokeMethod;
-import static org.wso2.carbon.identity.oauth.dcr.DCRMConstants.CLIENT_AUTHENTICATION_REQUIRED;
+import static org.wso2.carbon.identity.oauth.dcr.DCRMConstants.DCR_CONFIG_RESOURCE_NAME;
+import static org.wso2.carbon.identity.oauth.dcr.DCRMConstants.DCR_CONFIG_RESOURCE_TYPE_NAME;
 import static org.wso2.carbon.identity.oauth.dcr.DCRMConstants.ENABLE_FAPI_ENFORCEMENT;
-import static org.wso2.carbon.identity.oauth.dcr.DCRMConstants.SSA_JWKS;
 
 /**
  * Unit tests covering DCRConfigurationMgtServiceImpl class.
@@ -68,16 +60,17 @@ public class DCRConfigurationMgtServiceImplTest extends PowerMockTestCase {
 
     @BeforeMethod
     public void setUp() throws Exception {
+
         dcrConfigurationMgtService = new DCRConfigurationMgtServiceImpl();
         mockConfigurationManager = Mockito.mock(ConfigurationManager.class);
         DCRDataHolder.getInstance().setConfigurationManager(mockConfigurationManager);
+
     }
 
     @Test(priority = 1, description = "Test getDCRConfiguration method gets correct dcr configs")
     public void testGetDCRConfiguration() {
 
-        mockStatic(IdentityTenantUtil.class);
-        when(IdentityTenantUtil.getTenantId(anyString())).thenReturn(1);
+        mockStatic(IdentityUtil.class);
 
         try {
             Assert.assertTrue(invokeMethod(dcrConfigurationMgtService, "getDCRConfiguration")
@@ -98,55 +91,39 @@ public class DCRConfigurationMgtServiceImplTest extends PowerMockTestCase {
         }
     }
 
-    @Test(priority =  2, description = "Test setDCRConfiguration method sets DCRConfiguration properly" )
+    @Test(priority =  2, description = "Test setDCRConfiguration method properly sets DCRConfiguration" )
     public void testSetDCRConfiguration() {
 
+        mockStatic(IdentityUtil.class);
 
+        try {
+            dcrConfigurationMgtService.setDCRConfiguration(dcrConfiguration);
+            dcrConfiguration.setAuthenticationRequired(false);
+            dcrConfigurationMgtService.setDCRConfiguration(dcrConfiguration);
+
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof DCRMException);
+            Assert.assertEquals(DCRMConstants.DCRConfigErrorMessage.ERROR_CODE_SSA_NOT_MANDATED.getCode(),
+                    ((DCRMException) e).getErrorCode());
+        }
     }
 
-//    @Test(priority = 3, description = "Test overrideDCRServerConfigsWithDCRResourceConfig method properly overrides " +
-//            "the DCRConfiguration object")
-//    public void testOverrideDCRServerConfigsWithDCRResourceConfig() throws Exception {
-//
-//        Resource resource = new Resource();
-//        Attribute attribute1 = new Attribute(ENABLE_FAPI_ENFORCEMENT, "false");
-//        Attribute attribute2 = new Attribute(DCRMConstants.CLIENT_AUTHENTICATION_REQUIRED, "false");
-//        resource.setAttributes(Arrays.asList(attribute1, attribute2));
-//        resource.setHasAttribute(true);
-//
-//        invokeMethod(DCRConfigurationMgtServiceImpl.class,
-//                "overrideDCRServerConfigsWithDCRResourceConfig", resource, this.dcrConfiguration);
-//
-//        dcrConfigurationMgtService.
-//
-//        assertEquals(false, this.dcrConfiguration.getEnableFapiEnforcement());
-//        assertEquals(false, this.dcrConfiguration.getAuthenticationRequired());
-//    }
+    @Test(priority = 3, description = "Test overrideDCRServerConfigsWithDCRResourceConfig method properly overrides " +
+            "the DCRConfiguration object")
+    public void testOverrideDCRServerConfigsWithDCRResourceConfig() throws Exception {
 
-//    @Test(priority = 4, description = "Test parseConfig method properly converts the DCRConfiguration object to " +
-//            "a ResourceAdd object")
-//    public void testParseConfigurations() throws Exception {
-//
-//        ResourceAdd resourceAdd = invokeMethod(DCRConfigurationMgtServiceImpl.class,
-//                "parseConfig", this.dcrConfiguration);
-//
-//        List<Attribute> attributes = resourceAdd.getAttributes();
-//        Map<String, String> attributeMap = invokeMethod(DCRConfigurationMgtServiceImpl.class, "getAttributeMap",
-//                attributes);
-//
-//        String enableDCRFapiValue = attributeMap.get(ENABLE_FAPI_ENFORCEMENT);
-//        Boolean enableDCRFapi = enableDCRFapiValue != null ? Boolean.parseBoolean(enableDCRFapiValue) : null;
-//
-//        String clientAuthenticationRequiredValue = attributeMap.get(CLIENT_AUTHENTICATION_REQUIRED);
-//        Boolean clientAuthenticationRequired = clientAuthenticationRequiredValue != null ?
-//                Boolean.parseBoolean(clientAuthenticationRequiredValue) : null;
-//
-//        String ssaJwks = attributeMap.get(SSA_JWKS);
-//
-//        assertEquals(3, attributes.size());
-//        assertEquals(false, enableDCRFapi);
-//        assertEquals(false, clientAuthenticationRequired);
-//        assertEquals(dummySSAJwks, ssaJwks);
-//
-//    }
+        Resource resource = new Resource();
+        Attribute attribute1 = new Attribute(ENABLE_FAPI_ENFORCEMENT, "false");
+        Attribute attribute2 = new Attribute(DCRMConstants.CLIENT_AUTHENTICATION_REQUIRED, "true");
+        resource.setAttributes(Arrays.asList(attribute1, attribute2));
+        resource.setHasAttribute(true);
+
+        when(mockConfigurationManager.getResource(DCR_CONFIG_RESOURCE_TYPE_NAME, DCR_CONFIG_RESOURCE_NAME))
+                .thenReturn(resource);
+
+        dcrConfiguration = dcrConfigurationMgtService.getDCRConfiguration();
+
+        assertEquals(false, dcrConfiguration.getEnableFapiEnforcement());
+        assertEquals(true, dcrConfiguration.getAuthenticationRequired());
+    }
 }
