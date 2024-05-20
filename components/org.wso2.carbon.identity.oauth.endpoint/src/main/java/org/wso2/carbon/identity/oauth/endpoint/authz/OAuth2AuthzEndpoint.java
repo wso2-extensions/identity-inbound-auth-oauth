f06e96a527a87cfb2ca96d526a2c4af27060456f
vivekvinushanth <cvivekvinushanth@gmail.com>
2024-05-20 11:12:04 +0530

    update logic

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -347,17 +347,6 @@ public class OAuth2AuthzEndpoint {
             }
         }
 
-        if (StringUtils.isNotEmpty(oAuthMessage.getClientId())) {
-            try {
-                ServiceProvider serviceProvider = getServiceProvider(oAuthMessage.getClientId());
-                if (!serviceProvider.isApplicationAccessEnabled()) {
-                    return handleApplicationAccessDisabled(request);
-                }
-            } catch (OAuthSystemException e) {
-                return handleOAuthSystemException(oAuthMessage, e);
-            }
-        }
-
         try {
             // Start tenant domain flow if the tenant configuration is not enabled.
             if (!IdentityTenantUtil.isTenantedSessionsEnabled()) {
@@ -567,13 +556,6 @@ public class OAuth2AuthzEndpoint {
         return Response.status(HttpServletResponse.SC_FOUND).location(new URI(EndpointUtil.getErrorPageURL(request,
                 e.getErrorCode(), OAuth2ErrorCodes.OAuth2SubErrorCodes.INVALID_AUTHORIZATION_REQUEST,
                 e.getMessage(), null))).build();
-    }
-
-    private Response handleApplicationAccessDisabled(HttpServletRequest request) throws URISyntaxException {
-
-        return Response.status(HttpServletResponse.SC_FOUND).location(new URI(EndpointUtil.getErrorPageURL(request,
-                OAuth2ErrorCodes.ACCESS_DENIED, OAuth2ErrorCodes.OAuth2SubErrorCodes.ACCESS_DISABLED,
-               "Login access is disabled for the application", null))).build();
     }
 
     private Response handleOAuthProblemException(OAuthMessage oAuthMessage, OAuthProblemException e) throws
@@ -4700,6 +4682,11 @@ public class OAuth2AuthzEndpoint {
 
         return ApiAuthnUtils.buildResponseForAuthorizationFailure(
                 clientAttestationContext.getValidationFailureMessage(), log);
+    }
+
+    private Response handleAppAccessDisabledFailure() {
+
+        return ApiAuthnUtils.buildResponseForAuthorizationFailure("Application access is disabled.", log);
     }
 
     private Response handleUnsupportedGrantForApiBasedAuth() {
