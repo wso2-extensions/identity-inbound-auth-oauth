11df9f3efe39af2a31a55a399d0bdb2568488c5f
Thumimku <thumilan.15@cse.mrt.ac.lk>
2024-06-13 11:25:38 +0530

    improve error message for invalid user id

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/impersonation/validators/SubjectScopeValidator.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/impersonation/validators/SubjectScopeValidator.java
@@ -24,6 +24,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 import org.wso2.carbon.identity.oauth.common.OAuth2ErrorCodes;
+import org.wso2.carbon.identity.oauth2.IdentityOAuth2ClientException;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.authz.OAuthAuthzReqMessageContext;
 import org.wso2.carbon.identity.oauth2.impersonation.models.ImpersonationContext;
@@ -100,7 +101,13 @@ public class SubjectScopeValidator implements ImpersonationValidator {
             throw new IdentityOAuth2Exception(OAuth2ErrorCodes.INVALID_REQUEST,
                     "Use mapped local subject is mandatory but a local user couldn't be found");
         }
-        authenticatedUser = OAuth2Util.getUserFromUserName(username);
+        try {
+            authenticatedUser = OAuth2Util.getUserFromUserName(username);
+        } catch (IllegalArgumentException e) {
+            throw new IdentityOAuth2ClientException(OAuth2ErrorCodes.INVALID_REQUEST,
+                    "Invalid User Id provided for Impersonation request. Unable to find the user for given user id : "
+                            + subjectUserId + " tenant Domain : " + tenantDomain);
+        }
         authenticatedUser.setUserId(subjectUserId);
         authenticatedUser.setAuthenticatedSubjectIdentifier(subjectUserId);
         return authenticatedUser;
