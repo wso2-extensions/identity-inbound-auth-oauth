1b64bdc494d2b5f9cfc0cebe0f4e6c6a095ce969
Binara-Sachin <binara.sachin@gmail.com>
2024-09-04 06:47:06 +0530

    Remove whitespace

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/util/OAuth2Util.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/util/OAuth2Util.java
@@ -2840,7 +2840,7 @@ public class OAuth2Util {
                 log.debug("Error occurred while validating id token signature.");
             }
             return false;
-        }  catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error occurred while validating id token signature.");
             return false;
         }
