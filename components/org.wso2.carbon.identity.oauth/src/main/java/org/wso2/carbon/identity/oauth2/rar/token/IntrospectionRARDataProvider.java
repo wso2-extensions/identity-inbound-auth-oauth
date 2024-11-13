faa5e3136d6609b39117838443bdcf3ea2066ad6
vimukthiRajapaksha <reachvimukthi@gmail.com>
2024-11-13 20:08:37 +0530

    Fix refresh token introspection issue

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/rar/token/IntrospectionRARDataProvider.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/rar/token/IntrospectionRARDataProvider.java
@@ -1,5 +1,24 @@
+/*
+ * Copyright (c) 2024, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.carbon.identity.oauth2.rar.token;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.oauth.common.OAuthConstants;
@@ -22,6 +41,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import static org.wso2.carbon.identity.oauth2.rar.util.AuthorizationDetailsConstants.AUTHORIZATION_DETAILS;
+import static org.wso2.carbon.identity.oauth2.validators.RefreshTokenValidator.TOKEN_TYPE_NAME;
 
 /**
  * Class responsible for modifying the introspection response to include user-consented authorization details.
@@ -107,10 +127,8 @@ public class IntrospectionRARDataProvider implements IntrospectionDataProvider {
                     new OAuth2TokenValidationMessageContext(tokenValidationRequestDTO,
                             generateOAuth2TokenValidationResponseDTO(introspectionResponseDTO));
 
-            final AccessTokenDO accessTokenDO = OAuth2ServiceComponentHolder.getInstance().getTokenProvider()
-                    .getVerifiedAccessToken(tokenValidationRequestDTO.getAccessToken().getIdentifier(), false);
-
-            oAuth2TokenValidationMessageContext.addProperty(OAuthConstants.ACCESS_TOKEN_DO, accessTokenDO);
+            oAuth2TokenValidationMessageContext.addProperty(OAuthConstants.ACCESS_TOKEN_DO,
+                    this.getVerifiedToken(tokenValidationRequestDTO, introspectionResponseDTO));
 
             return oAuth2TokenValidationMessageContext;
         }
@@ -129,5 +147,18 @@ public class IntrospectionRARDataProvider implements IntrospectionDataProvider {
         tokenValidationResponseDTO.setExpiryTime(oAuth2IntrospectionResponseDTO.getExp());
 
         return tokenValidationResponseDTO;
+    }
+
+    private AccessTokenDO getVerifiedToken(final OAuth2TokenValidationRequestDTO tokenValidationRequestDTO,
+                                           final OAuth2IntrospectionResponseDTO introspectionResponseDTO)
+            throws IdentityOAuth2Exception {
+
+        if (StringUtils.equals(TOKEN_TYPE_NAME, introspectionResponseDTO.getTokenType())) {
+            return OAuth2ServiceComponentHolder.getInstance().getTokenProvider()
+                    .getVerifiedRefreshToken(tokenValidationRequestDTO.getAccessToken().getIdentifier());
+        } else {
+            return OAuth2ServiceComponentHolder.getInstance().getTokenProvider()
+                    .getVerifiedAccessToken(tokenValidationRequestDTO.getAccessToken().getIdentifier(), false);
+        }
     }
 }
