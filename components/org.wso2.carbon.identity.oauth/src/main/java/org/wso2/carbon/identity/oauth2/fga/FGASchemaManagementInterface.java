930d817da2798edc198486db5a7e1cd39dc3609a
Esala Mapa <150642840+EsalaMapa@users.noreply.github.com>
2025-01-03 12:43:16 +0530

    Add files that implement Fine-Grained Authorization.

---
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/fga/FGASchemaManagementInterface.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.identity.oauth2.fga;
+
+public interface FGASchemaManagementInterface {
+
+//    AbstractSchemaWriteResponse writeAuthzSchema(FGARequestInterface writeScehemaRequest);
+//    AbstractSchemaReadResponse readAuthzSchema(FGARequestInterface readSchemaRequest);
+}
