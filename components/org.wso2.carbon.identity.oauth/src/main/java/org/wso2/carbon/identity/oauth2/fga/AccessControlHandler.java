d6d1857d26b78dfac968b597053d608a07341540
Esala Mapa <150642840+EsalaMapa@users.noreply.github.com>
2025-01-03 12:41:13 +0530

    Remove Topaz implementation.

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/fga/AccessControlHandler.java
+++
@@ -1,9 +0,0 @@
-package org.wso2.carbon.identity.oauth2.fga;
-
-import java.util.List;
-
-public interface AccessControlHandler {
-
-   List<String> getAuthorized(List<String> fgaScopes, String userID);
-//   List<String> getScopes(AuthorizationRequest authorizationRequest);
-}
