9a04e812bd4efab03ada248a2ab456e7d969f726
LE Weerasinghe <liviruweera@gmail.com>
2025-02-20 17:59:48 +0530

    Add-amr conflict resolve

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -4206,6 +4206,21 @@ public class OAuth2AuthzEndpoint {
      */
     private List<String> getAMRValues(List<String> authMethods, Map<String, AuthenticatedIdPData> authenticatedIdPs) {
 
+        boolean authenticatorAMREnabled = true;
+        if (authenticatorAMREnabled) {
+            List<String> resultantAuthMethods = new ArrayList<>();
+            for (Map.Entry<String, AuthenticatedIdPData> entry : authenticatedIdPs.entrySet()) {
+                if (entry.getValue() != null && entry.getValue().getAuthenticators() != null) {
+                    for (AuthenticatorConfig authenticatorConfig : entry.getValue().getAuthenticators()) {
+                        if (authenticatorConfig != null && authenticatorConfig.getAmrValues() != null) {
+                            resultantAuthMethods.addAll(Arrays.asList(authenticatorConfig.getAmrValues()));
+                        }
+                    }
+                }
+            }
+            return resultantAuthMethods;
+        }//implement the logic using for loops
+
         boolean readAMRValueFromIdp = Boolean.parseBoolean(IdentityUtil.getProperty(
                 OAuthConstants.READ_AMR_VALUE_FROM_IDP));
         if (readAMRValueFromIdp) {
