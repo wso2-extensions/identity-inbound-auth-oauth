54b5098e77ba2201a74d855de9c3fa4c3c69ca81
ThaminduDilshan <thamindudill071@gmail.com>
2025-04-03 23:46:53 +0530

    Fix app tenant resolving during token revocation

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2Service.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/OAuth2Service.java
@@ -678,8 +678,15 @@ public class OAuth2Service extends AbstractAdmin {
 
                 } else if (accessTokenDO != null) {
                     if (revokeRequestDTO.getConsumerKey().equals(accessTokenDO.getConsumerKey())) {
+                        // Resolve the application tenant domain.
+                        String tenantDomain;
+                        if (StringUtils.isNotEmpty(PrivilegedCarbonContext.getThreadLocalCarbonContext()
+                                .getApplicationResidentOrganizationId())) {
+                            tenantDomain = IdentityTenantUtil.getTenantDomain(accessTokenDO.getTenantID());
+                        } else {
+                            tenantDomain = IdentityTenantUtil.getTenantDomain(IdentityTenantUtil.getLoginTenantId());
+                        }
                         // Extracting the application details with consumer key and tenant domain.
-                        String tenantDomain = IdentityTenantUtil.getTenantDomain(accessTokenDO.getTenantID());
                         if ((OAuth2Util.getAppInformationByClientId(accessTokenDO.getConsumerKey(), tenantDomain).
                                 isTokenBindingValidationEnabled()) && (!isValidTokenBinding(accessTokenDO.
                                 getTokenBinding(), revokeRequestDTO.getRequest()))) {
