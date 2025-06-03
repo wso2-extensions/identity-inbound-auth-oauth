2c6d98f0b5a3fc8728b9008ea8da2fd2327c7160
BimsaraBodaragama <bodaragamadb.office@gmail.com>
2025-06-04 04:04:13 +0530

    Handle in RegisterApiServiceImpl with config

--- components/org.wso2.carbon.identity.api.server.dcr/src/gen/java/org/wso2/carbon/identity/oauth2/dcr/endpoint/dto/ApplicationDTO.java
+++ components/org.wso2.carbon.identity.api.server.dcr/src/gen/java/org/wso2/carbon/identity/oauth2/dcr/endpoint/dto/ApplicationDTO.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.annotation.*;
 import javax.validation.constraints.NotNull;
 
 @ApiModel(description = "")
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApplicationDTO  {
   
   
