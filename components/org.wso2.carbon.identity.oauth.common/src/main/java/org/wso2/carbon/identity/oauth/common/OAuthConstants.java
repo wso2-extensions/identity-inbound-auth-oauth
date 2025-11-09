9f2e892a2ddb401b234808e8e7f3eaef0270bc7d
hisanhunais <hisan.live@gmail.com>
2025-11-09 16:35:41 +0530

    Add default values for secret ID and description

--- components/org.wso2.carbon.identity.oauth.common/src/main/java/org/wso2/carbon/identity/oauth/common/OAuthConstants.java
+++ components/org.wso2.carbon.identity.oauth.common/src/main/java/org/wso2/carbon/identity/oauth/common/OAuthConstants.java
@@ -183,6 +183,8 @@ public final class OAuthConstants {
     public static final String OPERATION_NOT_SUPPORTED_FOR_MULTIPLE_CLIENT_SECRET_MODE
             = "The requested operation is not supported as the multiple client secret support is disabled by server" +
             "configuration.";
+    public static final String SYSTEM_GENERATED_SECRET = "System generated secret";
+    public static final String DEFAULT_SECRET_ID = "Default_Secret_ID";
 
     private OAuthConstants() {
 
